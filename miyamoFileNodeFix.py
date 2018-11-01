4afa4b891e4bd5ba07efb055c2a3300145716a30
Miyamoto Shota <kikkichan@users.noreply.github.com>
2018-11-01 15:10:20 +0900

    bug fix

--- miyamoFileNodeFix.py
+++ miyamoFileNodeFix.py
@@ -1,39 +1,44 @@
+#------------
+#
+#last update 18/11/01
+#Fixed bug function stops when occured connectAttr error
+#
+#------------
+
 import maya.cmds as mc
 
-p2dAttrs=['coverage','translateFrame','rotateFrame','mirrorU','mirrorV','stagger','wrapU','wrapV','repeatUV','offset','rotateUV','noiseUV','vertexUvOne','vertexUvTwo','vertexUvThree','vertexCameraOne','outUV','outUvFilterSize']
-fileAttrs=['coverage','translateFrame','rotateFrame','mirrorU','mirrorV','stagger','wrapU','wrapV','repeatUV','offset','rotateUV','noiseUV','vertexUvOne','vertexUvTwo','vertexUvThree','vertexCameraOne','uvCoord','uvFilterSize']
+p2dAttrs = ['coverage','translateFrame','rotateFrame','mirrorU','mirrorV','stagger','wrapU','wrapV','repeatUV','offset','rotateUV','noiseUV','vertexUvOne','vertexUvTwo','vertexUvThree','vertexCameraOne','outUV','outUvFilterSize']
+fileAttrs = ['coverage','translateFrame','rotateFrame','mirrorU','mirrorV','stagger','wrapU','wrapV','repeatUV','offset','rotateUV','noiseUV','vertexUvOne','vertexUvTwo','vertexUvThree','vertexCameraOne','uvCoord','uvFilterSize']
 
 def reconnect_p2d(file, p2d):
-    f=file
-    p=p2d
-    #Reconnect file nodes to p2d node
-    for i in range(len(f)):
-        for attrIndex in range(len(fileAttrs)):
-            mc.connectAttr(p[0]+'.'+p2dAttrs[attrIndex], f[i]+'.'+fileAttrs[attrIndex], f=True)
-
-    #Display selected nodes in hypershade graph
+	f = file
+	p = p2d
+	#Reconnect file nodes to p2d node
+	for i in range(len(f)):
+		#To avoid error that already connected p2d to file node
+		if mc.isConnected(str(p[0]+'.'+p2dAttrs[0]), str(f[i]+'.'+fileAttrs[0])) == False:
+			#connect every attribute
+			for attrIndex in range(len(fileAttrs)):
+				mc.connectAttr(p[0]+'.'+p2dAttrs[attrIndex], f[i]+'.'+fileAttrs[attrIndex], f=True)
 
 def main():
-    obj=mc.ls(sl=True)
-    file=[]
-    p2d=[]
+	obj = mc.ls(sl=True)
+	file = []
+	p2d = []
 
-    #Separate Selected Nodes
-    for i in obj:
-        type=mc.nodeType(i)
-        if type=='file':
-            file.append(i)
-        if type=='place2dTexture':
-            p2d.append(i)
+	#Separate selected nodes file from p2d
+	for i in obj:
+		type = mc.nodeType(i)
+		if type == 'file':
+			file.append(i)
+		if type == 'place2dTexture':
+			p2d.append(i)
 
-    #return if no file node Selected
-    if len(file)==0:
-        return
 
-    #Create new p2d node if no p2d node selected
-    if len(p2d)==0:
-        p2d.append(mc.shadingNode('place2dTexture', n='p2d_1', au=True))
+	#Create new p2d node if no p2d nodes selected
+	if len(p2d) == 0:
+		p2d.append(mc.shadingNode('place2dTexture', n='p2d_1', au=True))
 
-    #reconnect progress
-    reconnect_p2d(file, p2d)
-    print('Reconnection Done')
+	#reconnect progress
+	reconnect_p2d(file, p2d)
+	print('Reconnection Done')
